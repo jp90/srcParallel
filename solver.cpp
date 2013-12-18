ee9796ea6739a240b92ea411fe386edd4c95024a
David <david@home-kreplin.de>
2013-12-18 15:03:19 +0100

    Comment Solver and Stencil

--- solver.cpp
+++ solver.cpp
@@ -10,9 +10,12 @@
 #include "math.h"
 using namespace std;
 
+//Using the default copy constructor of the SimIO class
 Solver::Solver(IO& SimIO) :
 		SimIO(SimIO) {
 }
+
+//computes residuum over a subspace
 RealType Solver::computeResidual(GridFunction& p,
 		GridFunction& rhs) {
 	PointType delta;
@@ -25,36 +28,32 @@ RealType Solver::computeResidual(GridFunction& p,
 	begin[1] = 1;
 	end[1] = p.griddimension[1] - 2;
 
+	//temporary gridfunctions
 	GridFunction branch_1(p.griddimension);
-	//branch_1.Grid_Print();
 	Pxx(branch_1, p, delta);
-	//branch_1.Grid_Print();
 	GridFunction branch_2(p.griddimension);
-	//branch_2.Grid_Print();
 	Pyy(branch_2, p, delta);
-	//branch_2.Grid_Print();
+
 
 	branch_1.AddToGridFunction(begin, end, 1.0, branch_2);
-	//branch_1.Grid_Print();
 	branch_1.AddToGridFunction(begin, end, -1.0, rhs);
-	//branch_1.Grid_Print();
 	branch_1.MultiplyGridFunctions(begin, end, branch_1);
-	//branch_1.Grid_Print();
 	branch_1.ScaleGridFunction(begin, end,
 			1.0 / (SimIO.para.iMax * SimIO.para.jMax));
 
+	//equation for the residuum
 	RealType res = 0.0;
 	for (int i = 1; i <= SimIO.para.iMax; i++) {
 		for (int j = 1; j <= SimIO.para.jMax; j++) {
-
 			res += branch_1.getGridFunction()[i][j];
 		}
 	}
 	return sqrt(res);
 
 }
-void Solver::SORCycle_Black(GridFunction& p, GridFunction& rhs) {
 
+//one cycle of the solver, starts with (1,2)
+void Solver::SORCycle_Black(GridFunction& p, GridFunction& rhs) {
 
 	for (int i = 1; i<=SimIO.para.iMax;i++){
 		for(int j=2-i%2; j<=SimIO.para.jMax;j+=2){
@@ -65,12 +64,11 @@ void Solver::SORCycle_Black(GridFunction& p, GridFunction& rhs) {
 						(p.getGridFunction()[i][j+1]+p.getGridFunction()[i][j-1])/(SimIO.para.deltaY*SimIO.para.deltaY)
 						- rhs.getGridFunction()[i][j]);
 
-
 		}
 	}
 
 }
-
+//one cycle of the solver, starts with (1,1)
 void Solver::SORCycle_White(GridFunction& p, GridFunction& rhs) {
 
 
@@ -82,7 +80,6 @@ void Solver::SORCycle_White(GridFunction& p, GridFunction& rhs) {
 						((p.getGridFunction()[i+1][j]+p.getGridFunction()[i-1][j])/(SimIO.para.deltaX*SimIO.para.deltaX) +
 						(p.getGridFunction()[i][j+1]+p.getGridFunction()[i][j-1])/(SimIO.para.deltaY*SimIO.para.deltaY)
 						- rhs.getGridFunction()[i][j]);
-
 
 		}
 	}
